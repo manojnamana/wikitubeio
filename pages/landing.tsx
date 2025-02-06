cc992de0359d399a152495ef4564c14ba8d17508
manojnamana <namanamanoj70@gmail.com>
2025-02-06 13:14:18 +0530

    google added

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -1,22 +1,5 @@
-// import { Button, Stack } from "@mui/material";
-// import router from 'next/router';
 
-// const Landing = () =>{
-//     const article_name = 'calculus'
-//     return(
-//         <>
-//     <Stack sx={{overflowY:"hidden"}}  >
-//         <img src="/static/images/creater.jpeg" alt="creater" style={{height:625}} width={"100%"}/>  
-//     </Stack>
-//      <Stack alignItems={"center"} bgcolor={"#0b0b0b"} >
-//      <Button onClick={()=>{router.push(`/wiki/${article_name}`)}}  variant="outlined" sx={{borderRadius:5,fontWeight:"bold",borderWidth:0,m:2,bgcolor:"white","&:hover":{bgcolor:"#3B71CA",color:"white"}}} >Remix With Web</Button>
-//      </Stack>
-//      </>
-//     )
-// }
-
-// export default Landing;
-import { useEffect } from "react";
+// @ts-nocheck
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
 import { Button, Stack } from "@mui/material";
@@ -25,17 +8,25 @@ const Landing = () => {
   const router = useRouter();
   const article_name = "calculus";
 
-  useEffect(() => {
-    // Extract token from URL
-    const urlParams = new URLSearchParams(window.location.search);
-    //const token = urlParams.get("token");
-    const token = document.cookie.split('; ').find(row => row.startsWith('token='));
+  // useEffect(() => {
+  //   // Extract token from URL
+  //   const urlParams = new URLSearchParams(window.location.search);
+  //   //const token = urlParams.get("token");
+  //   const token = document.cookie.split('; ').find(row => row.startsWith('token='));
 
-    if (token) {
-      Cookies.set("access_token", token, { expires: 1800, secure: true });
-      router.replace("/landing"); // Remove token from URL
-    }
-  }, [router]);
+  //   if (token) {
+  //     Cookies.set("access_token", token, { expires: 1800, secure: true });
+  //     router.replace("/landing"); // Remove token from URL
+  //   }
+  // }, [router]);
+
+
+  let access_token = router?.query?.token
+
+  if (access_token){
+    console.log(access_token)
+    Cookies.set('access_token', access_token, { expires: 7 });
+  }
 
   return (
     <>
